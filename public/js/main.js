43df6d643f00bd17112357669ab08c7030b30a02
Sam Sadtler <sam.sadtler@gmail.com>
2016-08-23 19:01:21 -0400

    simple socketio chat example

--- public/js/main.js
+++
@@ -1,9 +0,0 @@
-// CUSTOM JS FILE //
-
-console.log('start the party');
-var socket = io.connect();
-      socket.on('news', function (data) {
-        console.log(data);
-        socket.emit('my other event', { my: 'data' });
-    });
-console.log('end the party');
