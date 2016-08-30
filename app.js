d94dbe58df7b8da62a336748f3af12fac46e9603
Lacey Laborde <laceylaborde@gmail.com>
2016-08-30 14:42:29 -0400

    Lacey - reformat code

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var io = require('socket.io')(http);
 var fetch = require('node-fetch');
 fetch.Promise = require('promise');
 var FormData = require('form-data');
-	
+
 
 app.get('/', function(req, res){
   res.sendfile('index.html');
@@ -47,24 +47,19 @@ var lastTime = 0,
 var refreshTime = 1;
 var quakeTimer;
 
-function again(times){
-	console.log('maybe this is a good time to send a signal to the particle')
-  
-};
-
 function render() {
 	console.log('render');
   if(refreshTime == 1) {
    clearTimeout(quakeTimer);
    console.log('clearTimer')
    getQuakes();
-   
   };
 };
+
 function vibrateSense(magnitude, location){
 
 	console.log('in vibrateSense')
-	// this data is send to the Particle Electron
+	// this data is sent to the Particle Electron
 	// Code is stopping here
 	var form = new FormData();
 	form.append('args','on');
@@ -72,46 +67,43 @@ function vibrateSense(magnitude, location){
 	console.log('form --> ' + JSON.stringify(form))
 
 	fetch('https://api.particle.io/v1/devices/260044001951353338363036/led?access_token=28a267c1cb56bdbe8524907441b5b1ff0c92d2ca', { method: 'POST', body: form})
-	    .then(function(res) {
-	        return res.json();
-	    }).then(function(json) {
-	        console.log(json);
-	    });
-
-};
-function getQuakes() {
-fetch('http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson')
     .then(function(res) {
-
         return res.json();
-    }).then(function(data) {
+    })
+		.then(function(json) {
+        console.log(json);
+    });
+};
 
+function getQuakes() {
+	fetch('http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson')
+    .then(function(res) {
+			return res.json();
+		})
+		.then(function(data) {
       for(var i = 0; i < data.features.length; i++){
         var properties = data.features[i].properties;
         var time = properties.time;
         if(time > newTime){
-         newTime = time; 
-         magnitude = properties.mag;
-         place = properties.place;
-         console.log('time: ',newTime," magnitude: ", magnitude, " place: ", place);
+         	newTime = time;
+         	magnitude = properties.mag;
+         	place = properties.place;
+         	console.log('time: ',newTime," magnitude: ", magnitude, " place: ", place);
         }
       }
-      // console.log(newTime, lastTime);
+
       if(newTime > lastTime) {
         lastTime = newTime;
         console.log('There is a new quake! Its magnitude is ' + magnitude.toString());
        	vibrateSense(magnitude, place);
         // render();
       }
-    quakeTimer = setTimeout(function(){
-      	// console.log('call getQuakes again');
+
+    	quakeTimer = setTimeout(function(){
         getQuakes();
       }, refreshTime * 1000);
     })
 }
-
-
-
 
 setTimeout(function(){
   getQuakes();
