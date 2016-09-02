2dcab517c538871d9c7cc8216207d7be76d98817
Lacey Laborde <laceylaborde@gmail.com>
2016-09-01 21:56:37 -0400

    Lacey - refactor out methods

--- src/app.js
+++ src/app.js
@@ -1,11 +1,24 @@
-var lastTime = 0;
 var quakeTimer;
 
 function start() {
-	console.log('render');
+  checkForQuakes(0);
+}
+
+function stop() {
   clearTimeout(quakeTimer);
-  console.log('clearTimer')
-  getQuakes();
+}
+
+function checkForQuakes(lastTime) {
+  var mostRecentQuake = getMostRecentQuake();
+
+  if(mostRecentQuake.time > lastTime && mostRecentQuake.mag > 1) {
+    lastTime = mostRecentQuake.time;
+    var scaledMagnitude = scaleMagnitude(mostRecentQuake.mag);
+    var scaledDistanceToQuake = scaleDistance(getDistanceToQuake(mostRecentQuake.place, 'New+York,New+York'));
+    vibrateSense(scaledMagnitude, scaledDistanceToQuake);
+  }
+
+  quakeTimer = setTimeout(function(){ checkForQuakes(lastTime); }, 1000);
 }
 
 function vibrateSense(magnitude, location){
@@ -27,65 +40,41 @@ function vibrateSense(magnitude, location){
     });
 }
 
-function getQuakes() {
+function getMostRecentQuake() {
 	fetch('http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson')
     .then(function(res) {
 			return res.json();
 		})
 		.then(function(data) {
-      var mostRecentQuake = getMostRecentQuake(data)
-
-      if(mostRecentQuake.time > lastTime && mostRecentQuake.magnitude > 1) {
-        lastTime = mostRecentQuake.time;
-        console.log('There is a new quake! Its magnitude is ' + mostRecentQuake.magnitude.toString());
-        var scaledMagnitude = scaleMagnitude(mostRecentQuake.magnitude);
-        var scaledDistanceToQuake = getScaledDistanceToQuake(mostRecentQuake.place, 'New+York,New+York');
-        vibrateSense(scaledMagnitude, scaledDistanceToQuake);
-        render();
-      }
-
-    	quakeTimer = setTimeout(function(){ getQuakes(); }, 1000);
+      return data.features[0].properties;
     });
 }
 
-function getMostRecentQuake(data) {
-  if (data.features.length > 0) {
-  var properties = data.features[0].properties;
-    return {
-      "time" : properties.time,
-      "magnitude" : properties.mag,
-      "place" : properties.place
-    }
-  } else {
-    return {}
-  }
-}
-
-function getScaledDistanceToQuake(quakeAddress, deviceAddress) {
+function getDistanceToQuake(quakeAddress, deviceAddress) {
   quakeAddress = quakeAddress.split(' ').join('+');
   fetch('https://maps.googleapis.com/maps/api/geocode/json?origins='+quakeAddress+'&destinations='+deviceAddress+'&key='+process.env.GOOGLE_MAPS_API_KEY)
   .then(function(res) {
     return res.json();
   })
   .then(function(data) {
-    scaleDistance(data.rows[0].elements[0].distance.value);
+    return data.rows[0].elements[0].distance.value;
   })
 }
 
 function scaleDistance(distance) {
   var distanceMax = 20038000;
-  var scaleMax = 255;
+  var newMax = 255;
 
-  var scaledDistance = (scaleMax/distanceMax) * (distance - distanceMax) + scaleMax;
+  var scaledDistance = (newMax/distanceMax) * (distance - distanceMax) + newMax;
   return Math.abs(Math.round(scaledDistance));
 }
 
 function scaleMagnitude(magnitude) {
   var richterMax = 12;
   var richterMin = 1;
-  var milliMax = 3000;
-  var milliMin = 500;
+  var newMax = 3000;
+  var newMin = 500;
 
-  var scaledMagnitude = ((milliMax - milliMin)/(richterMax - richterMin)) * (magnitude - richterMax) + milliMax;
+  var scaledMagnitude = ((newMax - newMin)/(richterMax - richterMin)) * (magnitude - richterMax) + newMax;
   return Math.abs(Math.round(scaledMagnitude));
 }
--- test/spec/appSpec.js
+++ test/spec/appSpec.js
@@ -1,54 +1,20 @@
 describe("App.js", function() {
 
-  describe("Get most recent quake", function() {
-    describe("when there have been quakes in the past hour", function() {
-      it("gets the most recent quake", function() {
-        var data = { "features": [
-            {
-              "properties": {
-                "mag": 1.5,
-                "place": "6km WNW of The Geysers, California",
-                "time": 1472583453300
-              }
-            }
-          ]}
-
-        var result = getMostRecentQuake(data);
-
-        expect(result.magnitude).toEqual(1.5);
-        expect(result.place).toEqual("6km WNW of The Geysers, California");
-        expect(result.time).toEqual(1472583453300);
-      });
-    });
-
-    describe("when there have not been quakes in the past hour", function() {
-      it("returns empty json object", function() {
-        var data = { "features": []}
-
-        var result = getMostRecentQuake(data);
-
-        expect(result).toEqual({});
-      });
-    });
-  });
-
   describe("Scale magnitude", function() {
-
-    it("scales 1 to 500", function() {
+    it("scales 1 to 500 milliseconds", function() {
       expect(scaleMagnitude(1)).toEqual(500);
     });
 
-    it("scales 6.5 to 1750", function() {
+    it("scales 6.5 to 1750 milliseconds", function() {
       expect(scaleMagnitude(6.5)).toEqual(1750);
     });
 
-    it("scales 12 to 3000", function() {
+    it("scales 12 to 3000 milliseconds", function() {
       expect(scaleMagnitude(12)).toEqual(3000);
     });
   });
 
   describe("Scale distance", function() {
-
     it("scales 0 to 0", function() {
       expect(scaleDistance(0)).toEqual(0);
     });
@@ -61,6 +27,5 @@ describe("App.js", function() {
       expect(scaleDistance(20038000)).toEqual(255);
     });
   });
-
 
 })
