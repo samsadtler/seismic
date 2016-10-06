480bb5f51e7899672ccba7c84e64ebde0a85c4d3
Sam Sadtler <sam.sadtler@gmail.com>
2016-10-06 12:10:11 -0400

    setting up never sleep

--- src/app.js
+++ src/app.js
@@ -3,11 +3,16 @@ var express = require('express');
 require('es6-promise').polyfill();
 require('isomorphic-fetch');
 var formData = require('form-data');
+var http = require("http");
 
 var app = express();
 var port =  process.env.PORT || 5000;
 var quakeTimer;
 var lastRecordedQuakeTime = 0;
+
+setInterval(function() {
+    http.get("http://seismic-server.herokuapp.com");
+}, 300000)
 
 app.listen(port, function() {
   console.log('Server running on ' + port);
