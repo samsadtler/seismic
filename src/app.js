fb75010f0c1086569a4e4784abbc48953c781071
Andrew McWilliams <andy@jahya.net>
2016-11-29 12:49:53 -0500

    Cleans up console output

--- src/app.js
+++ src/app.js
@@ -13,34 +13,32 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'html');
 
-var port =  process.env.PORT || 4000;
+var port = process.env.PORT || 4000;
 var quakeTimer;
 var lastRecordedQuakeTime = 0;
 var recentLocation = {};
 
 setInterval(function() {
-    http.get("http://seismic-server.herokuapp.com");
+  http.get("http://seismic-server.herokuapp.com");
 }, 1500000);
 
 app.listen(port, function() {
-  console.log('Server running on ' + port);
+  console.log('Server running on port ' + port);
   dotenv.load();
   start();
 });
 
 app.get('/',function(req, res) {
   res.sendFile(path.join(__dirname+'/index.html'));
-
 });
 
 app.post('/api/location', function(req, res) {
-    res.send('POST request to the homepage');
-    console.log('location requested');
-    console.log(req.body);
+  res.send('POST request to the homepage');
+  console.log('location requested');
+  console.log(req.body);
 });
 
 function start() {
-  console.log('start monitoring');
   checkForQuakes();
 }
 
@@ -49,67 +47,67 @@ function stop() {
 }
 
 function checkForQuakes() {
-  loadMostRecentQuake()
-  .then(loadDistanceToQuake)
-  .then(triggerSense);
-
+  fetchNewQuakeData()
+    .then(loadDistance)
+    .then(respondToQuakeData);
   quakeTimer = setTimeout(function() { checkForQuakes(); }, 1000);
 }
 
-function loadMostRecentQuake() {
-	return fetch('http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson')
+function fetchNewQuakeData() {
+  return fetch('http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson')
     .then(function(res) {
-			return res.json();
-		})
-		.then(function(data) {
+      return res.json();
+    })
+    .then(function(data) {
       return data.features[0].properties;
     });
 }
 
-function loadDistanceToQuake(mostRecentQuake) {
+function loadDistance(quakeData) {
   //TODO: refactor this method
   //maximum number of api request 2500/day
-  quakeLocation = mostRecentQuake.place.split(' ').join('+');
+  quakeLocation = quakeData.place.split(' ').join('+');
 
   return fetch('https://maps.googleapis.com/maps/api/distancematrix/json?origins='+quakeLocation+'&destinations=New+York,New+York&key='+process.env.GOOGLE_MAPS_API_KEY)
   .then(function(res) {
     return res.json();
   })
   .then(function(data) {
-    mostRecentQuake.distance = 400000;
+    quakeData.distance = 400000;
     //faking distance until the google maps api chills
     //mostRecentQuake.distance = data.rows[0].elements[0].distance.value;
-    return mostRecentQuake;
+    return quakeData;
   });
 }
 
-function triggerSense(mostRecentQuake) {
-  if(mostRecentQuake.time > lastRecordedQuakeTime && mostRecentQuake.mag > 1) {
-    lastRecordedQuakeTime = mostRecentQuake.time;
-    var scaledMagnitude = scaleMagnitude(mostRecentQuake.mag);
-    var scaledDistanceToQuake = scaleDistance(mostRecentQuake.distance);
-    console.log("MAGNITUDE: " + mostRecentQuake.mag);
-    console.log(" DISTANCE: " + mostRecentQuake.distance/1000);
-    console.log("SCALED MAGNITUDE: " + scaledMagnitude);
-    console.log("SCALED DISTANCE: " + scaledDistanceToQuake);
-    vibrateSense(scaledMagnitude, scaledDistanceToQuake);
+function respondToQuakeData(quakeData) {
+  var isNewQuake = quakeData.time > lastRecordedQuakeTime;
+  var isHighMagnitude = quakeData.mag > 1;
+  var shouldVibrate = isNewQuake && isHighMagnitude;
+
+  if(shouldVibrate) {
+    lastRecordedQuakeTime = quakeData.time;
+    var scaledMagnitude = scaleMagnitude(quakeData.mag);
+    var scaledDistance = scaleDistance(quakeData.distance);
+    logShouldVibrate(quakeData, scaledMagnitude, scaledDistance);
+    vibrateSense(scaledMagnitude, scaledDistance);
   }
 }
 
 function vibrateSense(magnitude, distance) {
-  console.log('sending data');
-  var concatValues = magnitude+'n'+distance;
-  console.log('concatValues: ', concatValues);
+  var concatValues = magnitude + 'n' + distance;
+  console.log('Sending vibration command with values: ' + concatValues);
   var form = new formData();
-  form.append('args',concatValues);
+  form.append('args', concatValues);
 
   fetch('https://api.particle.io/v1/devices/'+process.env.DEVICE_KEY+'/data?access_token='+process.env.PARTICLE_TOKEN, { method: 'POST', body: form})
     .then(function(res) {
-        return res.json();
+      return res.json();
     })
     .then(function(json) {
-        console.log(json);
-  });
+      console.log('Response received: ');
+      console.log(json);
+    });
 }
 
 function scaleDistance(distance) {
@@ -128,4 +126,12 @@ function scaleMagnitude(magnitude) {
   var newMin = 200;
   var scaledMagnitude = ((newMax - newMin)/(richterMax - richterMin)) * (magnitude - richterMax) + newMax;
   return Math.abs(Math.round(scaledMagnitude));
+}
+
+function logShouldVibrate(quakeData, scaledMagnitude, scaledDistance){
+  console.log("Vibration triggered with values:");
+  console.log(" -> magnitude:        " + quakeData.mag);
+  console.log(" -> distance:         " + quakeData.distance);
+  console.log(" -> scaled magnitude: " + scaledMagnitude);
+  console.log(" -> scaled distance:  " + scaledDistance);
 }
