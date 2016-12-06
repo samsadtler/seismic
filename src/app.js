c56884b5f196dfc7b523e793e626cd53d0c8fe88
Sam Sadtler <sam.sadtler@gmail.com>
2016-12-06 17:54:12 -0500

    testing 6 added new Header

--- src/app.js
+++ src/app.js
@@ -92,7 +92,7 @@ function loadDistance(quakeData) {
 function loadCellTowerLocation(cellTowerData) {
   log('Loading distance from Google Geolocation API...');
   var headers = new Headers();
-  headers.set('Content-Type', 'application/json');
+  headers.append('Content-Type', 'application/json');
   var url = 'https://www.googleapis.com/geolocation/v1/geolocate?key='+process.env.GOOGLE_GEOLOCATION_API_KEY;
   fetch(url, {method: 'POST' , body: cellTowerData, headers: headers})
     .then(function(res) {
