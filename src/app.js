a798f12aa25eee673362a74aede54d1fa44424f4
Sam Sadtler <sam.sadtler@gmail.com>
2016-12-06 19:28:39 -0500

    testing 14 adjusting header

--- src/app.js
+++ src/app.js
@@ -96,10 +96,10 @@ function loadCellTowerLocation(cellTowerData) {
 
   var url = 'https://www.googleapis.com/geolocation/v1/geolocate?key='+process.env.GOOGLE_GEOLOCATION_API_KEY;
   fetch(url, {
-    'method': 'POST' , 
-    'headers': headers,
-    'body': cellTowerData 
-  }).then(function(res) {
+    method: 'POST' , 
+    headers: headers,
+    body: cellTowerData    
+ }).then(function(res) {
       log('Geolocation Response: ');
       log(res);
       log('   --> Header Content-Type: '+res.headers.get('Content-Type'))
